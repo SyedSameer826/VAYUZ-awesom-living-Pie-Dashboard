7c080a27059de6323a73b3e56fd315d1b7f22b1d
syedsameerhusain <syed.sameer.husain@vayuz.com>
2026-05-21 17:07:58 +0530

    Making pi Integration fixes.

--- frontend/src/pages/pairing/index.jsx
+++ frontend/src/pages/pairing/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io();
 
 function Pairing() {
   const [logs, setLogs] = useState([]);
